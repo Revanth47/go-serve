8c7c2d866105c611abffe81815e82c1455deff71
Revanth47 <7revanth47@gmail.com>
2016-07-03 19:43:53 +0530

    add option to serve file/directory

--- go-serve.go
+++ go-serve.go
@@ -29,12 +29,7 @@ func getServeConfig(args []string) ServeConfig {
 			if(err == nil){
 				config.Port = element
 			} else {
-			
 				if(element[0]!='/') {
-					pathStat,err := os.Stat(element)
-					if(err!=nil||!pathStat.IsDir()) {
-						log.Fatal("Invalid Directory Path")
-					}
 					config.Dir = element
 				}
 			}
@@ -46,12 +41,23 @@ func getServeConfig(args []string) ServeConfig {
 
 func serve(config ServeConfig) {
 
-	http.Handle(config.Path, http.FileServer(http.Dir(config.Dir)))
+	pathStat,err := os.Stat(config.Dir)
+	if(err!=nil) {
+		log.Fatal(err)
+	}
+
+	if(pathStat.IsDir()) {
+		http.Handle(config.Path, http.FileServer(http.Dir(config.Dir)))
+	} else {
+		http.HandleFunc(config.Path, func (w http.ResponseWriter, r *http.Request) {
+		    http.ServeFile(w, r, config.Dir)
+		})
+	}
 
 	fmt.Println("Serve Config \n Directory : " + path.Base(config.Dir) + " \n Path      : http://localhost:" + config.Port + config.Path + "\n")
 	log.Println("Starting server on port: " + config.Port)
 
-	err := http.ListenAndServe(":"+config.Port, nil)
+	err = http.ListenAndServe(":"+config.Port, nil)
 	if err != nil {
 		log.Fatal("Error ListenAndServe", err)
 	}
