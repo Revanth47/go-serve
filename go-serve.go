5b759302ee2ff474a34452e3f65939a5dac28bd1
Revanth47 <7revanth47@gmail.com>
2016-07-03 17:58:34 +0530

    add option to use custom port

--- go-serve.go
+++ go-serve.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strconv"
 )
 
 type ServeConfig struct {
@@ -14,16 +15,19 @@ type ServeConfig struct {
 	Path string
 }
 
-func main() {
-	args := os.Args[1:]
-	config := getServeConfig(args)
-	serve(config)
-}
-
 func getServeConfig(args []string) ServeConfig {
 
 	dir, _ := os.Getwd()
 	config := ServeConfig{"8000", dir, "/"}
+
+	if(len(args)>0){
+		for _,element := range args {
+			_,err := strconv.Atoi(element)
+			if(err == nil){
+				config.Port = element
+			}
+		}
+	}
 
 	return config
 }
@@ -32,11 +36,17 @@ func serve(config ServeConfig) {
 
 	http.Handle(config.Path, http.FileServer(http.Dir(config.Dir)))
 
-	fmt.Println("Serve Config \n Directory : " + path.Base(config.Dir) + " \n On        : http://localhost:" + config.Port + config.Path + "\n")
+	fmt.Println("Serve Config \n Directory : " + path.Base(config.Dir) + " \n Path      : http://localhost:" + config.Port + config.Path + "\n")
 	log.Println("Starting server on port: " + config.Port)
 
 	err := http.ListenAndServe(":"+config.Port, nil)
 	if err != nil {
 		log.Fatal("Error ListenAndServe", err)
 	}
+}
+
+func main() {
+	args := os.Args[1:]
+	config := getServeConfig(args)
+	serve(config)
 }
